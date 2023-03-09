6073e44ab6c338326659b40690d687fb47c15541
Paul <paul@lintcloud.com>
2023-03-09 16:38:06 +0530

    Project Initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Pagination from 'react-bootstrap/Pagination';
+import { Fragment, useEffect, useMemo, useState } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 function App() {
+
+  const Details = useMemo(() =>(
+    [
+      { id: 1, name: "A" }, { id: 2, name: 'B' }, { id: 3, name: "C" },
+      { id: 4, name: "A" }, { id: 5, name: 'B' }, { id: 6, name: "C" },
+      { id: 7, name: "A" }, { id: 8, name: 'B' }, { id: 9, name: "C" },
+      { id: 10, name: "A" }, { id: 11, name: 'B' }, { id: 12, name: "C" },
+      { id: 13, name: "A" }, { id: 14, name: 'B' }, { id: 15, name: "C" },
+    ]
+  ),[]);
+
+  const [data , setData] = useState([]);
+
+  const [active, setActive] = useState(1);
+  const paginationNumberItems = [];
+  for (let number = 1; number <= 10; number++) {
+    paginationNumberItems.push(
+      <Pagination.Item onClick={() => setActive(number)} key={number} active={number === active}>
+        {number}
+      </Pagination.Item>,
+    );
+  }
+
+  const getDataForThisPage = (id) =>{
+    const dataStart = parseInt(id -1) * 4 + 1;
+    const dataEnd = dataStart +3;
+
+    const filterDetails = Details.filter((data) => data.id >= dataStart && data.id <= dataEnd);
+
+    setData(filterDetails)
+
+  }
+
+  useEffect(() =>{
+    getDataForThisPage(active)
+  },[active])
+
+  const handlePrev = () => {
+    setActive(prev => prev === 1 ? 1 : prev - 1)
+  }
+
+  const handleNext = () => {
+    setActive(prev => prev === (paginationNumberItems.length) ? paginationNumberItems.length : prev + 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Row>
+        {data.length ? (
+          data.map((elm) => (
+            <Fragment key={elm.id}>
+              <Col lg={3}>{elm.name}</Col>
+            </Fragment>
+          ))
+        ) : (
+          <Col>
+            <h1>No Data</h1>
+          </Col>
+        )}
+      </Row>
+
+
+
+
+      <Pagination>
+        <Pagination.Prev onClick={handlePrev} />
+        {paginationNumberItems}
+        <Pagination.Next onClick={handleNext} />
+      </Pagination>
     </div>
   );
 }
